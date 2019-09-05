857dfc3e332b7a122153a6ff70f05b3d70c076ed
daniella0726 <daniella0726@gmail.com>
2019-09-05 14:59:17 -0500

    Docentes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_133339) do
+ActiveRecord::Schema.define(version: 2019_09_05_195253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "docentes", force: :cascade do |t|
+    t.string "p1"
+    t.string "p2a"
+    t.string "p2b"
+    t.string "p3"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "estudiantes", force: :cascade do |t|
     t.string "p1"
