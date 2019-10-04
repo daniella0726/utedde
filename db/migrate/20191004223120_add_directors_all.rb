d89962e3a1437fb5cee90ba816c6d58bbf54e988
daniella0726 <daniella0726@gmail.com>
2019-10-04 18:16:46 -0500

    Campos directores y actualización vista directores y docentes + controller

---
+++ db/migrate/20191004223120_add_directors_all.rb
@@ -0,0 +1,250 @@
+class AddDirectorsAll < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :directors, :p2c, :string
+
+  	add_column :directors, :p11_1, :string
+  	add_column :directors, :p11_2, :string
+  	add_column :directors, :p11_3, :string
+  	add_column :directors, :p11_4, :string
+  	add_column :directors, :p11a, :string
+
+  	add_column :directors, :p12_1, :string
+  	add_column :directors, :p12_2, :string
+  	add_column :directors, :p12_3, :string
+  	add_column :directors, :p12_4, :string
+  	add_column :directors, :p12_5, :string
+  	add_column :directors, :p12_6, :string
+  	add_column :directors, :p12_7, :string
+  	add_column :directors, :p12_8, :string
+  	add_column :directors, :p12_9, :string
+  	add_column :directors, :p12_10, :string
+  	add_column :directors, :p12_11, :string
+  	add_column :directors, :p12_12, :string
+  	add_column :directors, :p12_13, :string
+  	add_column :directors, :p12_14, :string
+  	add_column :directors, :p12_15, :string
+  	add_column :directors, :p12_16, :string
+  	add_column :directors, :p12_17, :string
+  	add_column :directors, :p12_18, :string
+  	add_column :directors, :p12a, :string
+
+  	add_column :directors, :p13_1, :string
+  	add_column :directors, :p13_2, :string
+  	add_column :directors, :p13_3, :string
+  	add_column :directors, :p13_4, :string
+  	add_column :directors, :p13_5, :string
+  	add_column :directors, :p13_6, :string
+  	add_column :directors, :p13_7, :string
+  	add_column :directors, :p13_8, :string
+  	add_column :directors, :p13_9, :string
+  	add_column :directors, :p13_10, :string
+  	add_column :directors, :p13a, :string
+
+  	add_column :directors, :p14_1, :string
+  	add_column :directors, :p14_2, :string
+  	add_column :directors, :p14_3, :string
+  	add_column :directors, :p14_4, :string
+  	add_column :directors, :p14_5, :string
+  	add_column :directors, :p14_6, :string
+  	add_column :directors, :p14_7, :string
+  	add_column :directors, :p14_8, :string
+  	add_column :directors, :p14_9, :string
+  	add_column :directors, :p14_10, :string
+  	add_column :directors, :p14a, :string
+
+  	add_column :directors, :p15_1, :string
+  	add_column :directors, :p15_2, :string
+  	add_column :directors, :p15_3, :string
+  	add_column :directors, :p15_4, :string
+  	add_column :directors, :p15_5, :string
+  	add_column :directors, :p15_6, :string
+  	add_column :directors, :p15_7, :string
+  	add_column :directors, :p15_8, :string
+  	add_column :directors, :p15_9, :string
+  	add_column :directors, :p15_10, :string
+  	add_column :directors, :p15_11, :string
+  	add_column :directors, :p15_12, :string
+  	add_column :directors, :p15_13, :string
+  	add_column :directors, :p15_14, :string
+  	add_column :directors, :p15_15, :string
+  	add_column :directors, :p15a, :string
+
+  	add_column :directors, :p16_1, :string
+  	add_column :directors, :p16_2, :string
+  	add_column :directors, :p16_3, :string
+  	add_column :directors, :p16_4, :string
+  	add_column :directors, :p16_5, :string
+  	add_column :directors, :p16_6, :string
+  	add_column :directors, :p16a, :string
+
+  	add_column :directors, :p17_1, :string
+  	add_column :directors, :p17_2, :string
+  	add_column :directors, :p17_3, :string
+  	add_column :directors, :p17_4, :string
+  	add_column :directors, :p17a, :string
+
+  	add_column :directors, :p18_1, :string
+  	add_column :directors, :p18_2, :string
+  	add_column :directors, :p18_3, :string
+  	add_column :directors, :p18_4, :string
+  	add_column :directors, :p18_5, :string
+  	add_column :directors, :p18_6, :string
+  	add_column :directors, :p18a, :string
+
+  	add_column :directors, :p19, :string
+  	add_column :directors, :p19a_1, :string
+  	add_column :directors, :p19a_2, :string
+  	add_column :directors, :p19a_3, :string
+  	add_column :directors, :p19a_4, :string
+  	add_column :directors, :p19a_5, :string
+  	add_column :directors, :p19b, :string
+
+  	add_column :directors, :p20, :string
+  	add_column :directors, :p20a_1, :string
+  	add_column :directors, :p20a_2, :string
+
+  	add_column :directors, :p21_1, :string
+  	add_column :directors, :p21_2, :string
+  	add_column :directors, :p21_3, :string
+  	add_column :directors, :p21_4, :string
+  	add_column :directors, :p21_5, :string
+  	add_column :directors, :p21_6, :string
+  	add_column :directors, :p21_7, :string
+  	add_column :directors, :p21_8, :string
+
+  	add_column :directors, :p22_1, :string
+  	add_column :directors, :p22_2, :string
+  	add_column :directors, :p22_3, :string
+  	add_column :directors, :p22_4, :string
+  	add_column :directors, :p22_5, :string
+
+  	add_column :directors, :p23_1, :string
+  	add_column :directors, :p23_2, :string
+  	add_column :directors, :p23_3, :string
+  	add_column :directors, :p23_4, :string
+  	add_column :directors, :p23_5, :string
+
+  	add_column :directors, :p24_1, :string
+  	add_column :directors, :p24_2, :string
+  	add_column :directors, :p24_3, :string
+  	add_column :directors, :p24_4, :string
+  	add_column :directors, :p24_5, :string
+  	add_column :directors, :p24_6, :string
+
+  	add_column :directors, :p25_1, :string
+  	add_column :directors, :p25_2, :string
+  	add_column :directors, :p25_3, :string
+  	add_column :directors, :p25_4, :string
+
+  	add_column :directors, :p26_1, :string
+  	add_column :directors, :p26_2, :string
+  	add_column :directors, :p26_3, :string
+  	add_column :directors, :p26_4, :string
+  	add_column :directors, :p26_5, :string
+  	add_column :directors, :p26_6, :string
+  	add_column :directors, :p26_7, :string
+  	add_column :directors, :p26_8, :string
+  	add_column :directors, :p26_9, :string
+  	add_column :directors, :p26_10, :string
+
+  	add_column :directors, :p27, :string
+  	add_column :directors, :p27a_1, :string
+  	add_column :directors, :p27a_2, :string
+  	add_column :directors, :p27a_3, :string
+  	add_column :directors, :p27a_4, :string
+  	add_column :directors, :p27a_5, :string
+  	add_column :directors, :p27a_6, :string
+  	add_column :directors, :p27a_7, :string
+  	add_column :directors, :p27a_8, :string
+  	add_column :directors, :p27a_9, :string
+  	add_column :directors, :p27a_10, :string
+  	add_column :directors, :p27a_11, :string
+  	add_column :directors, :p27a_12, :string
+  	add_column :directors, :p27a_13, :string
+
+  	add_column :directors, :p28_1, :string
+  	add_column :directors, :p28_2, :string
+  	add_column :directors, :p28_3, :string
+  	add_column :directors, :p28_4, :string
+  	add_column :directors, :p28_5, :string
+  	add_column :directors, :p28_6, :string
+  	add_column :directors, :p28_7, :string
+  	add_column :directors, :p28_8, :string
+  	add_column :directors, :p28_9, :string
+  	add_column :directors, :p28a, :string
+
+  	add_column :directors, :p29_1, :string
+  	add_column :directors, :p29_2, :string
+  	add_column :directors, :p29_3, :string
+  	add_column :directors, :p29_4, :string
+  	add_column :directors, :p29_5, :string
+  	add_column :directors, :p29_6, :string
+  	add_column :directors, :p29_7, :string
+  	add_column :directors, :p29_8, :string
+  	add_column :directors, :p29_9, :string
+
+  	add_column :directors, :p30_1, :string
+  	add_column :directors, :p30_2, :string
+  	add_column :directors, :p30_3, :string
+  	add_column :directors, :p30_4, :string
+  	add_column :directors, :p30_5, :string
+  	add_column :directors, :p30_6, :string
+  	add_column :directors, :p30_7, :string
+  	add_column :directors, :p30_8, :string
+
+  	add_column :directors, :p31_1, :string
+  	add_column :directors, :p31_2, :string
+  	add_column :directors, :p31_3, :string
+  	add_column :directors, :p31_4, :string
+  	add_column :directors, :p31_5, :string
+  	add_column :directors, :p31_6, :string
+  	add_column :directors, :p31_7, :string
+  	add_column :directors, :p31_8, :string
+  	add_column :directors, :p31_9, :string
+  	add_column :directors, :p31_10, :string
+  	add_column :directors, :p31_11, :string
+  	add_column :directors, :p31_12, :string
+  	add_column :directors, :p31_13, :string
+  	add_column :directors, :p31_14, :string
+  	add_column :directors, :p31_15, :string
+  	add_column :directors, :p31_16, :string
+  	add_column :directors, :p31a, :string
+
+  	add_column :directors, :p32_1, :string
+  	add_column :directors, :p32_2, :string
+  	add_column :directors, :p32_3, :string
+  	add_column :directors, :p32_4, :string
+  	add_column :directors, :p32_5, :string
+  	add_column :directors, :p32_6, :string
+  	add_column :directors, :p32_7, :string
+  	add_column :directors, :p32_8, :string
+  	add_column :directors, :p32_9, :string
+  	add_column :directors, :p32a, :string
+
+  	add_column :directors, :p33_1, :string
+  	add_column :directors, :p33_2, :string
+  	add_column :directors, :p33_3, :string
+  	add_column :directors, :p33_4, :string
+  	add_column :directors, :p33_5, :string
+  	add_column :directors, :p33_6, :string
+  	add_column :directors, :p33_7, :string
+  	add_column :directors, :p33_8, :string
+  	add_column :directors, :p33_9, :string
+  	add_column :directors, :p33_10, :string
+  	add_column :directors, :p33_11, :string
+  	add_column :directors, :p33_12, :string
+  	add_column :directors, :p33_13, :string
+  	add_column :directors, :p33_14, :string
+  	add_column :directors, :p33_15, :string
+  	add_column :directors, :p33_16, :string
+  	add_column :directors, :p33a, :string
+
+  	add_column :directors, :p34, :string
+  	add_column :directors, :p34a, :string
+
+  	add_column :directors, :p35, :string
+
+  	add_column :directors, :pen, :string
+
+  end
+end
