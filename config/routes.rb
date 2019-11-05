d440c808e29a6bb4b37901fce278452555fc07f9
daniella0726 <daniella0726@gmail.com>
2019-11-05 11:43:33 -0500

    ingresar a gráficos desde el index

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'home#index'
+
 get 'grafs_controller/graficos1'
 get 'grafs_controller/graficos2'
 
