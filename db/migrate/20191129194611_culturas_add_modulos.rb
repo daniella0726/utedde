8d77db3fa14dabeef666fb163899997b653fc2d1
daniella0726 <daniella0726@gmail.com>
2019-11-29 14:47:56 -0500

    Correcciones sin el modulo 4

---
+++ db/migrate/20191129194611_culturas_add_modulos.rb
@@ -0,0 +1,4 @@
+class CulturasAddModulos < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_171147) do
+ActiveRecord::Schema.define(version: 2019_11_29_194611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
