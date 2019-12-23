c1dd7e53ab91c6b5c70558dcec5edd27055d5f53
daniella0726 <daniella0726@gmail.com>
2019-12-23 09:54:09 -0500

    Cultura1 sin index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_134605) do
+ActiveRecord::Schema.define(version: 2019_12_23_120441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abierta", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cultura1s", force: :cascade do |t|
     t.datetime "created_at", null: false
